5b25168ce94157c7095ddfe361a830747b2dbe4c
Ana Paula Proaño Puente <proanoana336@gmail.com>
2026-08-23 15:06:02 -0500

    Primer commit

---
+++ tarea_semana10.py
@@ -0,0 +1,7 @@
+matriz = [[25,30,35], 
+          [100,110,120], 
+          [350,400,450]]
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        print(matriz[i][j])
+        
---
+++ tarea_semana9.py
@@ -0,0 +1,4 @@
+arreglo = [10,20,30,40,50]
+print("valor en posición 1:" , arreglo [1])
+for numero in arreglo:
+  print(numero)
